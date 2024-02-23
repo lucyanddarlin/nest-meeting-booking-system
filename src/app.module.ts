45b4bba6e81d012b1db80792df743f77984beaaa
lucy.li <lucy.li@quectel.com>
2024-02-23 10:45:30 +0800

    feat: 增加 .env 配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,36 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    // 引入配置
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/.env',
+    }),
     // 引入 TypeOrm
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'lucy',
-      database: 'meeting_room_booking_system',
-      synchronize: true,
-      logging: true,
-      entities: [],
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      extra: {
-        authPlugin: 'sha256_password',
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      async useFactory(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: configService.get('mysql_server_host'),
+          username: configService.get('mysql_server_username'),
+          password: configService.get('mysql_server_password'),
+          database: configService.get('mysql_server_database'),
+          poolSize: configService.get('mysql_server_pool_size'),
+          connectorPackage: configService.get('mysql_server_package'),
+          synchronize: true,
+          logging: true,
+          entities: [],
+          extra: {
+            authPlugin: 'sha256_password',
+          },
+        };
       },
     }),
     UserModule,
